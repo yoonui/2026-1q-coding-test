6c6ee341a616f3772d2a67ef360d26417530420d
yoonui <yoony0612@naver.com>
2026-01-24 12:53:55 +0900

    Lv.2 - 행렬의 곱셈

---
+++ 1월/260124_2_행렬의_곱셈.py
@@ -0,0 +1,16 @@
+def solution(arr1, arr2):
+    answer = []
+
+    for i in range(len(arr1)):
+        tmpArr = []
+        for j in range(len(arr2[0])):
+            sum = 0
+            for k in range(len(arr1[0])):
+                sum += arr1[i][k] * arr2[k][j]
+            tmpArr.append(sum)
+        answer.append(tmpArr)
+        
+    return answer
+
+print(solution([[1, 4], [3, 2], [4, 1]], [[3, 3], [3, 3]]))
+print(solution([[2, 3, 2], [4, 2, 4], [3, 1, 4]], [[5, 4, 3], [2, 4, 1], [3, 1, 1]]))
